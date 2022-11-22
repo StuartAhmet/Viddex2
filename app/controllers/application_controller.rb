d85b0131028a1228d0dbf22372448fd180c8f4d7
StuartAhmet <sma0190@gmail.com>
2022-11-22 12:15:35 +0000

    initial setup, setup devise, user models, navbar, some views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def after_sign_in_path_for(user)
+    user_url(user)
+  end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :company_name, :industry])
+  end
+
+
+
+  def after_sign_up_path_for(user)
+    user_url(user)
+  end
 end
