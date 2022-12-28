31d89b622e86d0caf4407a772bf95f18ea508cef
StuartAhmet <sma0190@gmail.com>
2022-12-28 16:27:22 +0000

    copy buttons now working for projects

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,7 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+//= require copy
+import "./copy"
+import "./controllers/copy_image_controller.js"
---
+++ app/javascript/copy.js
@@ -0,0 +1,39 @@
+// copy.js
+
+const imageContainers = document.querySelectorAll('.image-container');
+
+imageContainers.forEach(imageContainer => {
+  const copyButton = imageContainer.querySelector('#copy-button');
+  const image = imageContainer.querySelector('#image');
+  const imageLink = imageContainer.querySelector('#image-link');
+
+  copyButton.addEventListener('click', () => {
+    // Create a new div element
+    const copyDiv = document.createElement('div');
+
+    // Create copies of the image and its hyperlink
+    const imageCopy = image.cloneNode(true);
+    const imageLinkCopy = imageLink.cloneNode(true);
+
+    // Append the copies of the image and its hyperlink to the div
+    // copyDiv.appendChild(imageCopy);
+    copyDiv.appendChild(imageLinkCopy);
+
+    // Select the contents of the div
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(copyDiv);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    // Copy the contents of the div to the clipboard
+    navigator.clipboard.write([
+      new ClipboardItem({
+        'text/html': new Blob([copyDiv.innerHTML], { type: 'text/html' })
+      })
+    ]);
+
+    // Clear the selection
+    selection.removeAllRanges();
+  });
+});
