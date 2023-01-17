6b2814e117e7acffd154e48c24e6d74ac5d9f868
Cameron Ahmet <td.ahmet@gmail.com>
2023-01-17 22:55:57 +1100

    started the contact page - semi finished the full screen view and added all of the little photo/background changes we talked about before

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   }
   root to: "pages#home"
   get "contact", to: "pages#contact"
+  get 'contactus', to: 'pages#contactus'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
