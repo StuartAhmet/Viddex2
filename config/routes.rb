871264de453547dce70a1f853e33c47c3c0ed324
StuartAhmet <sma0190@gmail.com>
2023-02-02 14:02:56 +0000

    changed the name from backgrounds to images. this is only changed as a name in Views. the model, schema etc and all things related to backend, the name is still backgrounds.

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   patch '/contacts/:id', to: 'contacts#update'
 
   resources :users, :path => 'terminal' do
-    resources :backgrounds
+    resources :backgrounds, :path => 'images'
     resources :projects, only: [:new, :create, :edit, :update, :index, :destroy]
     resources :videos
     resources :audios, :path => 'audio-files'
