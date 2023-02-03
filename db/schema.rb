1eb0e776c98056045becb49e74011f4ae1a136d0
Cameron Ahmet <td.ahmet@gmail.com>
2023-02-03 16:36:41 +1100

    finished navbar essentials

--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_114335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
-  end
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -76,7 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_114335) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.jsonb "distort", default: {}
     t.integer "distort_nw_x"
     t.integer "distort_nw_y"
     t.integer "distort_ne_x"
