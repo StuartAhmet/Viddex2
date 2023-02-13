5e063abf1b9ab06c14e4a9dbfabb377d39736e86
Cameron Ahmet <td.ahmet@gmail.com>
2023-02-13 17:13:37 +1100

    who we are page

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   root to: "pages#home"
-  get 'contactus', to: 'pages#contactus'
+  get 'contactus', to: 'pages#contactus'    # to get to 'contact us' page
   get 'myadmin', to: 'admin#admin'
-  get 'pricing', to: 'pages#pricing'
-  get 'who_we_are', to: 'pages#who_we_are'
-  get 'what_we_do', to: 'pages#what_we_do'
+  get 'pricing', to: 'pages#pricing'        # to get to 'pricing' page
+  get 'who_we_are', to: 'pages#who_we_are'  # to get to 'who we are' page
+  get 'what_we_do', to: 'pages#what_we_do'  # to get to 'what we do' page
   post '/contactus', to: 'contacts#create'
   get 'contacts/new' => 'contacts#new'
   get '/contacts/:id', to: 'contacts#show'
